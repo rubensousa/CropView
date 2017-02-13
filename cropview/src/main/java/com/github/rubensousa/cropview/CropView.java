695155c1461451e74404bc7aab18b5c3215147bb
ruben <rubensousa.mieti@gmail.com>
2017-02-13 13:25:34 +0000

    Start drawing frame

--- cropview/src/main/java/com/github/rubensousa/cropview/CropView.java
+++ cropview/src/main/java/com/github/rubensousa/cropview/CropView.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.TypedValue;
@@ -19,6 +20,8 @@ public class CropView extends FrameLayout {
     private Canvas canvas;
     private Bitmap bitmap;
     private Paint paint;
+    private Paint framePaint;
+    private int touchPointWidth;
     private CropViewDelegate delegate;
     private int backgroundColor;
     private int frameColor;
@@ -47,6 +50,7 @@ public class CropView extends FrameLayout {
         backgroundColor = a.getColor(R.styleable.CropView_cropViewFrameColor,
                 ContextCompat.getColor(context, R.color.cropview_default_color));
 
+        touchPointWidth = getResources().getDimensionPixelOffset(R.dimen.cropview_touchpoint_width);
         a.recycle();
 
         delegate = new CropViewDelegate(this,
@@ -56,6 +60,9 @@ public class CropView extends FrameLayout {
         paint = new Paint();
         paint.setColor(0xFFFFFFFF);
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        framePaint = new Paint();
+        framePaint.setAntiAlias(true);
+        framePaint.setColor(frameColor);
         setWillNotDraw(false);
         setBackground(null);
     }
@@ -70,20 +77,30 @@ public class CropView extends FrameLayout {
     @Override
     protected void onDraw(Canvas canvas) {
         bitmap.eraseColor(Color.TRANSPARENT);
-
         this.canvas.drawColor(backgroundColor);
 
         // Clear background where the frame is
         this.canvas.drawRect(delegate.getCropRect(), paint);
 
         canvas.drawBitmap(bitmap, 0, 0, null);
-    }
-
-    private void drawOverlay(Canvas canvas) {
-
+        drawFrame(canvas);
     }
 
     private void drawFrame(Canvas canvas) {
+        Rect rect = delegate.getCropRect();
 
+        framePaint.setStrokeWidth(touchPointWidth / 4);
+        // Draw lines between touch points
+        canvas.drawLine(rect.left, rect.top, rect.right, rect.top, framePaint);
+        canvas.drawLine(rect.left, rect.top, rect.left, rect.bottom, framePaint);
+        canvas.drawLine(rect.right, rect.top, rect.right, rect.bottom, framePaint);
+        canvas.drawLine(rect.left, rect.bottom, rect.right, rect.bottom, framePaint);
+
+        framePaint.setStrokeWidth(0);
+        // Draw touch points
+        canvas.drawCircle(rect.left, rect.top, touchPointWidth, framePaint);
+        canvas.drawCircle(rect.right, rect.top, touchPointWidth, framePaint);
+        canvas.drawCircle(rect.left, rect.bottom, touchPointWidth, framePaint);
+        canvas.drawCircle(rect.right, rect.bottom, touchPointWidth, framePaint);
     }
 }
